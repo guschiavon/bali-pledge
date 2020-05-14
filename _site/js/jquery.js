9b18cf2183e7412ddf08ef373d8e21cd1f1d03c6
Gus <gustavo.schiavon@gmail.com>
2020-05-14 10:51:38 +0800

    MQ for styles

--- _site/js/jquery.js
+++ _site/js/jquery.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function(){
+
  console.log('jQuery loaded')
 
   $('.burger').click(function(){
--- _site/js/pledge-script.js
+++ _site/js/pledge-script.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
   console.log('Pledge Loaded')
 
-
   $('.pledge-button').click(function(){
     $(this).toggleClass('active');
 
     console.log('Button Clicked')
 
+
     if ( $('#pledge-button-1').is('.active') && $('#pledge-button-2').is('.active') && $('#pledge-button-3').is('.active') ){
       console.log('Form Active')
 
-      $('#pledge-form').removeClass("hide");
+      $('#pledge-form').fadeIn(1000);
     }
     else if ( !$('#pledge-button-1').is('.active') || !$('#pledge-button-2').is('.active') || !$('#pledge-button-3').is('.active') ){
       console.log('Form Hidden')
 
-      $('#pledge-form').addClass("hide");
+      $('#pledge-form').fadeOut(500);
     }
   });
 });
